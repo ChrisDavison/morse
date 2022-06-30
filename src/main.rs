8fc497a951316d2cff9922cdf3eb2f8a7c272621
Chris Davison <c.jr.davison@gmail.com>
2022-06-30 23:05:22 +0100

    Use single LUT for morse and char

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
-const LUT: [&str; 36] = [
-    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
-    "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
-    ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----",
-];
+type MorseLUTPair<'a> = (&'a str, char);
 
-const LETTERS: [char; 36] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+const LUT: [MorseLUTPair; 36] = [
+    (".-", 'a'),
+    ("-...", 'b'),
+    ("-.-.", 'c'),
+    ("-..", 'd'),
+    (".", 'e'),
+    ("..-.", 'f'),
+    ("--.", 'g'),
+    ("....", 'h'),
+    ("..", 'i'),
+    (".---", 'j'),
+    ("-.-", 'k'),
+    (".-..", 'l'),
+    ("--", 'm'),
+    ("-.", 'n'),
+    ("---", 'o'),
+    (".--.", 'p'),
+    ("--.-", 'q'),
+    (".-.", 'r'),
+    ("...", 's'),
+    ("-", 't'),
+    ("..-", 'u'),
+    ("...-", 'v'),
+    (".--", 'w'),
+    ("-..-", 'x'),
+    ("-.--", 'y'),
+    ("--..", 'z'),
+    (".----", '1'),
+    ("..---", '2'),
+    ("...--", '3'),
+    ("....-", '4'),
+    (".....", '5'),
+    ("-....", '6'),
+    ("--...", '7'),
+    ("---..", '8'),
+    ("----.", '9'),
+    ("-----", '0'),
 ];
 
 const WORD_SPACE: &str = "       ";
@@ -18,21 +48,21 @@ fn main() {
     // If the binary is called 'morse', convert a sentence into morse
     // If it's called unmorse, convert morse into a sentence
     let output = match args[0].as_ref() {
-        "morse" => morse(words),
-        "unmorse" => unmorse(words),
+        "morse" => from_morse(words),
+        "unmorse" => to_morse(words),
         _ => String::new(),
     };
     println!("{output}");
 }
 
-fn unmorse(sentence: &str) -> String {
+fn to_morse(sentence: &str) -> String {
     let mut words = Vec::new();
     sentence.split(' ').into_iter().for_each(|word| {
         let unmorsed = word
             .chars()
             .into_iter()
-            .flat_map(|letter| LETTERS.iter().position(|x| x == &letter))
-            .map(|pos| LUT[pos].to_string())
+            .flat_map(|letter| LUT.iter().position(|(_morse, ch)| ch == &letter))
+            .map(|pos| LUT[pos].0.to_string())
             .collect::<Vec<String>>()
             .join(" ")
             .trim()
@@ -42,12 +72,12 @@ fn unmorse(sentence: &str) -> String {
     words.join(WORD_SPACE).trim().to_string()
 }
 
-fn morse(sentence: &str) -> String {
+fn from_morse(sentence: &str) -> String {
     let mut vars = Vec::new();
     for word in sentence.split(WORD_SPACE) {
         for letter in word.split(' ') {
-            if let Some(pos) = LUT.iter().position(|x| x == &letter) {
-                vars.push(LETTERS[pos].to_string());
+            if let Some(pos) = LUT.iter().position(|(morse, _ch)| morse == &letter) {
+                vars.push(LUT[pos].1.to_string());
             }
         }
         vars.push(String::from(" "));
@@ -61,28 +91,28 @@ mod tests {
 
     #[test]
     fn valid_words_test() {
-        assert_eq!(morse("."), "e");
-        assert_eq!(morse("..-. ..- -.-. -.-"), "fuck");
+        assert_eq!(from_morse("."), "e");
+        assert_eq!(from_morse("..-. ..- -.-. -.-"), "fuck");
         assert_eq!(
-            morse("- .... .. ...       .. ...       .-       ... . -. - . -. -.-. ."),
+            from_morse("- .... .. ...       .. ...       .-       ... . -. - . -. -.-. ."),
             "this is a sentence"
         );
-        assert_eq!(morse(".----"), "1");
+        assert_eq!(from_morse(".----"), "1");
     }
 
     #[test]
     fn strip_invalid_chars_test() {
-        assert_eq!(morse("///"), "");
+        assert_eq!(from_morse("///"), "");
     }
 
     #[test]
     fn unmorse_test() {
-        assert_eq!(unmorse("e"), ".");
-        assert_eq!(unmorse("fuck"), "..-. ..- -.-. -.-");
+        assert_eq!(to_morse("e"), ".");
+        assert_eq!(to_morse("fuck"), "..-. ..- -.-. -.-");
         assert_eq!(
-            unmorse("this is a sentence"),
+            to_morse("this is a sentence"),
             "- .... .. ...       .. ...       .-       ... . -. - . -. -.-. .",
         );
-        assert_eq!(unmorse("1"), ".----");
+        assert_eq!(to_morse("1"), ".----");
     }
 }
